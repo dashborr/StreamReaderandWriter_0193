349daf717269a2a55f0e0879414ce2aede0d6a72
dashborr <danisarfas7@gmail.com>
2026-06-01 16:35:58 +0700

    menambahkan class TokoElektronik

--- praktikum.cpp
+++ praktikum.cpp
@@ -4,3 +4,34 @@
 #include <array>
 #include <vector>
 #include <stdexcept>
+
+using namespace std;
+
+class TokoElektronik
+{
+private:
+    array<string, 3> etalase;
+
+public:
+    TokoElektronik()
+    {
+        etalase[0] = "Laptop ASUS";
+        etalase[1] = "Smartphone Samsung";
+        etalase[2] = "Printer Epson";
+    }
+
+    string ambilProduk(size_t nomorRak)
+    {
+        try
+        {
+            return etalase.at(nomorRak);
+        }
+        catch (const out_of_range&)
+        {
+            throw string(
+                "Gagal Mengambil Barang : Rak nomor " +
+                to_string(nomorRak) +
+                " kosong atau tidak tersedia!");
+        }
+    }
+};
